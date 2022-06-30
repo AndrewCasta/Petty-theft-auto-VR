c7de43b3f59a7ced9f6a384a9166dd7e85cd9d69
Andrew Casta <acasta4704@gmail.com>
2022-06-30 17:10:43 +1000

    Update velocity tracked grabbable
    Add property for original parent interactable and set the object back to original parent on release.

--- Assets/Scripts/XRGrabVelocityTracked.cs
+++ Assets/Scripts/XRGrabVelocityTracked.cs
@@ -1,26 +1,35 @@
+using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class XRGrabVelocityTracked : XRGrabInteractable
 {
+    Transform originalParent;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        originalParent = transform.parent;
+    }
+
     protected override void OnSelectEntered(SelectEnterEventArgs interactor)
     {
-        SetParentToXRRig();
+        SetParentToInteractor();
         base.OnSelectEntered(interactor);
     }
 
     protected override void OnSelectExited(SelectExitEventArgs interactor)
     {
-        SetParentToWorld();
+        SetParentToOriginal();
         base.OnSelectExited(interactor);
     }
 
-    public void SetParentToXRRig()
+    public void SetParentToInteractor()
     {
         transform.SetParent(interactorsSelecting[0].transform);
     }
 
-    public void SetParentToWorld()
+    public void SetParentToOriginal()
     {
-        transform.SetParent(null);
+        transform.SetParent(originalParent);
     }
 }
